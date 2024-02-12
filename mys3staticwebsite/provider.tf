4f0845f2fd2de1f6621a416c0eedee9f2172151c
Mohamed Nishath <117710744+nishathmhd@users.noreply.github.com>
2024-02-12 20:59:44 +0530

    Add files via upload

---
+++ mys3staticwebsite/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.32.1"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
---
+++ mys3staticwebsite/variables.tf
@@ -0,0 +1,3 @@
+variable "bucketname" {
+  default = "myterraformprojectwebsite-2024"
+}
